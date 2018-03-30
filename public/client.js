4e2a3843bc48527fdfc47212abd85cd60bc36364
Glitch (program-by-voice) <none>
2018-03-30 04:20:10 +0000

    Updated with Glitch - cleanup

--- public/client.js
+++
@@ -1,7 +0,0 @@
-// client-side js
-// run by the browser each time your view template is loaded
-
-// by default, you've got jQuery,
-// add other scripts at the bottom of index.html
-
-$(function() {});
